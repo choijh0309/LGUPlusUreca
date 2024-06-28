c2bffbf98ef7fcf4249c6f0b617bbe0c4d4dd9ca
JUNHA CHOI <choijh0309@naver.com>
2024-06-28 10:44:33 +0900

    Sorting Z update

--- Algo/src/bj/Bj_Z_1074_2.java
+++ Algo/src/bj/Bj_Z_1074_2.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class bj_Z_1074_2 {
+public class Bj_Z_1074_2 {
 
 	static int N, r, c, ans;
 	public static void main(String[] args) throws Exception {
